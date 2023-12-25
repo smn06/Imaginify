4a1e4b322ba18d591f32738910d1e018f4c6f47b
Sultan Mahmud Nahian <sultanmahmud621@gmail.com>
2023-12-25 20:45:44 +0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Imaginify is an innovative project that explores the fascinating realm of Text-to-Image Generation using Generative Adversarial Networks (GANs). This system empowers users to transform textual descriptions into vivid, lifelike images, pushing the boundaries of creativity and imagination.',
+    author='smn06',
+    license='MIT',
+)
